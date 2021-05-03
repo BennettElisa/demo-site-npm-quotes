be9d83ad59fbe2d89fe8ccdb3c21bd0e4fd757c1
Elisa <bilingualedu@gmail.com>
2021-05-02 22:34:34 -0400

    Fix: close gaps within code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from "./components/header"
 import Inspiration from "./components/inspiration"
 import LeftView from "./components/leftView"
@@ -8,22 +7,19 @@ import Features from "./components/features"
 import Footer from "./components/footer"
 import CallToAction from "./components/call-to-action"
 
-
 function App() {
   return (
     <div className="App" id="container-fluid">
-          <Header/>
-          <Inspiration/>
-          <LeftView/>
-          <CarouselSlide/>
-          <RightView/>
-          <Features/>
-          <CallToAction/>
-          <Footer/>
-
-
+      <Header />
+      <Inspiration />
+      <LeftView />
+      <CarouselSlide />
+      <RightView />
+      <Features />
+      <CallToAction />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
